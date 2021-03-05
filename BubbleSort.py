a1d86ac6f0f44a9b199524a3e541d19b8ca5f25d
nbala02 <63129564+nbala02@users.noreply.github.com>
2021-03-05 01:15:52 -0500

    Create BubbleSort.py

---
+++ BubbleSort.py
@@ -0,0 +1,12 @@
+def bubbleSort(values):
+  print(values)
+  for i in range(len(values)-1):
+    for j in range (0, len(values)-1):
+      if values[j] > values[j+1]:
+        temp = values[j]
+        values[j] = values[j+1]
+        values[j+1] = temp
+  print(values)
+
+values = [8,3,5,2,4,9,7,0]
+bubbleSort(values)
